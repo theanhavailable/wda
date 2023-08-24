251d4e7c02c39fb5581248ec59e4b550e007fd4a
theanhavailable <141305436+theanhavailable@users.noreply.github.com>
2023-08-25 00:20:50 +0700

    Add files via upload

---
+++ js/backtotop.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+// JavaScript Document
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+var typed = new Typed('.element', {
+               	strings: ["Thiết kế đồ họa","Nhiếp ảnh","Làm chuyển động","Làm Phim"],
+       	 	typeSpeed:50,
+        		backSpeed:10,
+        		loop:true,
+        		});
+
+//end of text write
+$('.counter').countUp();
+new WOW().init();
---
+++ js/slide.js
@@ -0,0 +1,28 @@
+$('#gallery').owlCarousel({
+    loop:true,
+    margin:10,
+	autoplay:true,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('#cam-nhan').owlCarousel({
+loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+ })
+
+
